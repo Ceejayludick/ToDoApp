76445feca2cdf82bdf1f11314900a26586861bab
Ceejayludick <ceejayludick13@gmail.com>
2023-03-29 12:42:05 +0200

    Update to js
    Changes to JS due to functionality issues when adding new code

--- js/todoapp.js
+++ js/todoapp.js
@@ -1,6 +1,6 @@
 
 
-// Create a "close" button and append it to each list item
+// code to make a delete/close for every list item
 const myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
@@ -11,7 +11,7 @@ for (i = 0; i < myNodelist.length; i++) {
   myNodelist[i].appendChild(span);
 }
 
-// Click on a close button to hide the current list item
+// code to press close/delete on list item
 const close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
@@ -22,12 +22,9 @@ for (i = 0; i < close.length; i++) {
 }
 
 // Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+
+
+// code for saving input values 
 
 
 
